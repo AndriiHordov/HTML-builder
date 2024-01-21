d5735d6bdb12f27c5d3c32abe2cda9d62030b3bc
AndriiHordov <146883659+AndriiHordov@users.noreply.github.com>
2024-01-21 16:22:07 +0100

    feat: add script for storage data from standard input in directory 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+const output = fs.createWriteStream(
+  path.join(__dirname, 'output.txt'),
+  'utf-8',
+);
+stdout.write('Enter data to storing: \n');
+stdin.on('data', (data) => {
+  const buffer = data.toString().trim();
+  if (buffer === 'exit') {
+    process.exit();
+  } else {
+    output.write(data, 'utf-8');
+  }
+});
+process.on('exit', () => {
+  output.close();
+  console.log('File modified succesfull!! \nShutting down...');
+});
