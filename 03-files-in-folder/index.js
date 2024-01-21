921ace33154d92a91717278af643085a0fadf211
AndriiHordov <146883659+AndriiHordov@users.noreply.github.com>
2024-01-21 19:50:48 +0100

    feat: add script for displaying information about files, stored in folder 03-flies-in-folder/secret-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const fsPromices = require('fs/promises');
+const path = require('path');
+const { stdout } = process;
+const folderPath = path.join(__dirname, 'secret-folder');
+const ONE_KB = 1024;
+const viewer = (name, ext, size) =>
+  stdout.write(`${name} - ${ext} - ${size ? `${size}kb` : size} \n`);
+fsPromices
+  .readdir(folderPath, {
+    withFileTypes: true,
+    recursive: true,
+  })
+  .then((resolve) => {
+    resolve.forEach((file) =>
+      fs.stat(path.join(folderPath, file.name), (err, stats) => {
+        if (err) {
+          throw err;
+        }
+        if (!file.isDirectory()) {
+          const dotIndex = file.name.indexOf('.');
+          const name = file.name.slice(0, dotIndex);
+          const ext = file.name.slice(dotIndex + 1);
+          const sizeInKB = (+stats['size'] / ONE_KB).toFixed(3);
+          viewer(name, ext, sizeInKB);
+        }
+      }),
+    );
+  });
